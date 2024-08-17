cc7f86cb7a683b9bb2a5ff7c6c0e65cb6da5f750
shekhars4700 <shekhars4700@gmail.com>
2024-08-16 20:16:00 -0500

    MaxArea

---
+++ LeetCode/Two Pointer/MaxArea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Two_Pointer
+{
+    public partial class TwoPointer
+    {
+        public int MaxArea(int[] height)
+        {
+            int res = 0, area = 0, left = 0, right = height.Length - 1;
+
+            while (left < right)
+            {
+
+                area = (Math.Min(height[left], height[right])) * (right - left);
+                res = Math.Max(area, res);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+
+            }
+            return res;
+        }
+    }
+}
